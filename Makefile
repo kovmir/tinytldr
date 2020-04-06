4d1a279711a611ae4f0d9f04b8c3e673864cf0b7
unInstance <uninstance@protonmail.com>
2020-04-06 14:19:12 +0200

    Add project files

---
+++ Makefile
@@ -0,0 +1,45 @@
+CC = cc
+
+CFLAGS  = -std=c99
+CFLAGS += -pedantic
+CFLAGS += -Wall
+CFLAGS += -Wextra
+CFLAGS += -Wcast-align
+CFLAGS += -Wstrict-prototypes
+CFLAGS += -Wundef
+
+CFLAGS += -D_DEFAULT_SOURCE
+
+LDFLAGS  = -lcurl
+LDFLAGS += -larchive
+
+PREFIX = /usr/local
+
+PROJECT = tldr
+
+all:
+	$(CC) $(CFLAGS) $(LDFLAGS) *.c -o $(PROJECT)
+
+debug:
+	$(CC) $(CFLAGS) $(LDFLAGS) -g *.c -o $(PROJECT)
+
+gdb: debug
+	gdb $(PROJECT)
+
+memcheck: debug
+	valgrind --leak-check=yes ./$(PROJECT)
+
+memcheck_v: debug
+	valgrind --leak-check=yes -v ./$(PROJECT)
+
+memcheck_full: debug
+	valgrind --leak-check=full --show-leak-kinds=all ./$(PROJECT)
+
+
+clean:
+	rm $(PROJECT)
+
+install:
+	cp $(PROJECT) $(PREFIX)/bin/$(PROJECT)
+
+.PHONY: all debug gdb memcheck memcheck_v memcheck_full clean
---
+++ config.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+
+/* Path to store man pages relative to $HOME */
+static const char *PAGES_PATH = "/.config/tldr";
+
+/* Pages language. Uncomment ONE of the following*/
+static const char *PAGES_LANG = "/pages"; /* English */
+/* static const char *PAGES_LANG = "/pages.de"; */
+/* static const char *PAGES_LANG = "/pages.es"; */
+/* static const char *PAGES_LANG = "/pages.fr"; */
+/* static const char *PAGES_LANG = "/pages.hbs"; */
+/* static const char *PAGES_LANG = "/pages.it"; */
+/* static const char *PAGES_LANG = "/pages.ja"; */
+/* static const char *PAGES_LANG = "/pages.ko"; */
+/* static const char *PAGES_LANG = "/pages.pt_BR"; */
+/* static const char *PAGES_LANG = "/pages.pt_PT"; */
+/* static const char *PAGES_LANG = "/pages.ta"; */
+/* static const char *PAGES_LANG = "/pages.zh"; */
+
+/* Colors and styling */
+static const char *HEADING_STYLE = "\033[31m";
+static const char *SUBHEADING_STYLE = "\033[4m";
+static const char *COMMAND_STYLE = "\033[1m";
