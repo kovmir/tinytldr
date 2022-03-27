7e5c6925f5c8b4846aedc0acacd02ba9d3b8650a
Ivan Kovmir <uninstance@darkn.space>
2022-03-27 10:08:20 +0200

    Update copyright year

--- tldr.c
+++ tldr.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Ivan Kovmir
+ * Copyright 2022 Ivan Kovmir
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
