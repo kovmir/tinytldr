41a993e65783dcf614bcc3e64e96bf1ba0f39618
Ivan Kovmir <i@kovmir.eu>
2026-08-07 14:25:46 +0200

    Add config.h commets

--- config.h
+++ config.h
@@ -1,13 +1,15 @@
+/* RECOMPILE AFTER EDITS! */
+
 /* URL to download the archive with man pages. */
 static const char *PAGES_URL = "https://github.com/tldr-pages/tldr/releases/download/v2.3/tldr-pages.en.zip";
 /* Path to store man pages. */
 static const char *PAGES_HOME = "~/.local/share/tinytldr/pages";
+
 /* Print empty lines from pages? */
 static const int SKIP_EMPTY = 1;
 /* Apply ANSI styling to pages? */
 static const int APPLY_STYLES = 1;
-/* Page styling.
- * See: https://en.wikipedia.org/wiki/ANSI_escape_code */
+/* Page styling; see https://en.wikipedia.org/wiki/ANSI_escape_code */
 static const char *HEADING_STYLE = "\033[31m";
 static const char *SUMMARY_STYLE = "\033[22;4m";
 static const char *COMMENT_STYLE = "\033[22;32m";
